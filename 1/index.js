e58bd55199e6dc67cb46fc6fa7b86f961794e2f5
Tom C <thomaspchristian@outlook.com>
2022-07-05 21:28:57 -0400

    3 is broken

---
+++ 1/index.js
@@ -0,0 +1,6 @@
+function createDiv() {
+    $(document).ready(function(){
+    let div = document.createElement('div')
+    $('body').append(div)
+   });
+}
---
+++ 2/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function setName() {
+    
+        $('#buttonName').click(function() {
+            $('input[name="name"').val('Tom C')
+        });
+});
+    
+
+/* 
+
+$(document).ready(function () {
+    $('#buttonName').click(function () {
+        $('input[name="name"]').val('William');
+    });
+}); 
+
+*/
